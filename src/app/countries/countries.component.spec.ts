7bbd17a4f8dc2b5f59e997c366ee46740e08fe8f
Olha Popovych <bartimeusurucskiy@gmail.com>
2018-02-02 01:05:12 +0200

    fix test

--- src/app/countries/countries.component.spec.ts
+++ src/app/countries/countries.component.spec.ts
@@ -18,8 +18,4 @@ describe('CountriesComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- src/app/location-weather/location-weather.component.spec.ts
+++ src/app/location-weather/location-weather.component.spec.ts
@@ -18,8 +18,4 @@ describe('LocationWeatherComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
